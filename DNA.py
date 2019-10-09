042e31be7d0e705869aa23ba06e7cf8019a3149e
Mark Mersnik <markmersnik@gmail.com>
2019-10-09 10:52:36 -0700

    Adjustments made to create class. checkPop() class added.

--- DNA.py
+++ DNA.py
@@ -1,13 +1,14 @@
 import random
 import string 
 
+
 class DNA :
 
+	#Constructs our 
 	def __init__(self):
 		self.genes = []
 		self.fitnessScore = 0.0
-
-		for i in range(0, 18):
+		for i in range(0, 2):
 			self.genes.append(random.choice(string.ascii_letters + " "))
 
 
@@ -18,7 +19,7 @@ class DNA :
 				score += 1
 		self.fitnessScore = score/len(Setup().target)
 
-	#crossover between child and parent DNA
+	#Crossover between child and parent DNA
 	def mate(self, parent):
 		child = DNA()
 		midpoint = random.randint(0, len(self.genes)-1)
@@ -38,54 +39,67 @@ class DNA :
 		phrase = ""
 		for i in self.genes:
 			phrase += i
+
 		return phrase
 
 class Setup:
 
 	def __init__(self):
-		self.mutationRate = 0.01
-		self.totalPopulation = 200
+		self.mutationRate = 0.05
+		self.totalPopulation = 500
 		self.population = []
 		self.matingPool = []
-		self.target = "to be or not to be"
+		self.target = "to"
 		self.targetHit = ""
 		self.complete = False
+		self.count = 1
 
 		for i in range(0, self.totalPopulation):
 			self.population.append(DNA())
 
 	def create(self):
+		
+		#This is where CheckPop() was.
+		if(self.complete == False):
+			for i in range(0, self.totalPopulation):
+				#print(str(self.count) + ") ", end="")
+				phrase = self.population[i].checkString()
+				#print(phrase)
+				self.isCompleted(phrase)
+				self.count += 1
+				n = int(self.population[i].fitnessScore * 100)
+
+				for j in range(0, n):
+					self.matingPool.append(self.population[i])
+
+			#Reproduction
+			for i in range(0, len(self.population)):
+				matingPoolLength = len(self.matingPool)
+
+				if(matingPoolLength > 0):
+					a = random.randint(0, matingPoolLength - 1)
+					b = random.randint(0, matingPoolLength - 1)
+
+					partnerA = self.matingPool[a]
+					partnerB = self.matingPool[b]
+					
+					child = partnerA.mate(partnerB)
+					child.mutate()
+
+					self.population[i] = child
+		else:
+			print("Done")
+
+
+	def checkPop():
 		for i in range(0, len(self.population)):
 			self.population[i].fitness()
 			self.targetHit = self.population[i].checkString()
-			print(self.targetHit)
 			self.isCompleted(self.targetHit)
 			#print(self.population[i].fitnessScore)
 
-		for i in range(0, self.totalPopulation):
-			n = int(self.population[i].fitnessScore * 100)
-
-			for j in range(0, n):
-				self.matingPool.append(self.population[i])
-
-		#Reproduction
-		for i in range(0, len(self.population)):
-			matingPoolLength = len(self.matingPool)
-
-			if(matingPoolLength > 0):
-				a = random.randint(0, matingPoolLength - 1)
-				b = random.randint(0, matingPoolLength - 1)
-
-				partnerA = self.matingPool[a]
-				partnerB = self.matingPool[b]
-				
-				child = partnerA.mate(partnerB)
-				child.mutate()
-
-				self.population[i] = child
-
 	def isCompleted(self, phrase):
-		if(phrase == self.target):
+		if(phrase is self.target):
 			self.complete = True
 
 
@@ -93,4 +107,5 @@ class Setup:
 
 d = Setup()
 while(d.complete == False):
+	print(str(d.count + d.complete))
 	d.create()
